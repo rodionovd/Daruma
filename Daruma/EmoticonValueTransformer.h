e78a737e2382d784a116e648b6989b100c9f63a6
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2016-02-19 20:17:52 +0600

    Fix a typo

--- Daruma/EmoticonValueTransformer.h
+++ Daruma/EmoticonValueTransformer.h
@@ -9,7 +9,7 @@
 @import Foundation;
 
 /// Inserts newlines as appropriate for the better look.
-/// This tranformer is utilized by the FeelEmoticon.xib's TextField <-> model binding
+/// This tranformer is utilized by the FeelEmoticonView <-> model binding
 @interface EmoticonValueTransformer : NSValueTransformer
 
 - (NSString *)transformedValueForCopying: (NSString *)value;
