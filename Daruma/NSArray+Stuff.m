f5d53eb6f58711781e0626fd3b7b288e7949d371
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2016-02-21 22:15:33 +0600

    Add more NSArray helpers
    * filter
    * each
    * distinct (remove duplicates)

--- Daruma/NSArray+Stuff.m
+++ Daruma/NSArray+Stuff.m
@@ -19,6 +19,27 @@
     return result;
 }
 
+- (NSArray *)rd_filterWithBlock:(BOOL (^)(id))block
+{
+    NSMutableArray *new = [NSMutableArray array];
+    [self enumerateObjectsUsingBlock: ^(id obj, NSUInteger idx, BOOL *stop) {
+        if (block(obj)) [new addObject: obj];
+    }];
+    return new;
+}
+
+- (void)rd_each: (nonnull void (^)(id _Nonnull obj))block
+{
+    [self enumerateObjectsUsingBlock: ^(id _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        block(obj);
+    }];
+}
+
+- (nonnull NSArray *)rd_distinct
+{
+    return [[NSOrderedSet orderedSetWithArray: self] array];
+}
+
 - (nullable id)rd_randomItem
 {
     return self.count == 0 ? nil : self[arc4random_uniform((uint32_t)self.count)];
